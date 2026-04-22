d186a6187ce919df6d5066817097ac5f7e78a383
Aidan Walker <aidan.walker@amexgbt.com>
2026-04-22 15:25:45 +0100

    Client auth setup to prevent unhandled client error message

--- TimeTracker.Client/Program.cs
+++ TimeTracker.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TimeTracker.Client;
 using TimeTracker.Client.Services;
+using TimeTracker.Shared.Entities;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7078/") });
 
 await builder.Build().RunAsync();
---
+++ TimeTracker.Client/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+using TimeTracker.Shared.Models.Account;
+
+namespace TimeTracker.Client.Services
+{
+    public interface IAuthService
+    {
+        Task Register(AccountRegistrationRequest request);
+    }
+}
